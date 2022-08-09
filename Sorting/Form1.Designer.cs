1f77ad01c019f418e53de410337f24550dc41f1f
char-kyeopta <charisse.oblefias01@gmail.com>
2022-08-09 11:12:26 +0800

    Add project files.

---
+++ Sorting/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlCustomize = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.cboAlgorithms = new System.Windows.Forms.ComboBox();
+            this.lblAlgorithms = new System.Windows.Forms.Label();
+            this.btnAddNote = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.pnlSimulation = new System.Windows.Forms.Panel();
+            this.bgwBubbleSort = new System.ComponentModel.BackgroundWorker();
+            this.bgwSelectionSort = new System.ComponentModel.BackgroundWorker();
+            this.bgwQuickSort = new System.ComponentModel.BackgroundWorker();
+            this.bgwInsertionSort = new System.ComponentModel.BackgroundWorker();
+            this.pnlCustomize.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlCustomize
+            // 
+            this.pnlCustomize.BackColor = System.Drawing.Color.White;
+            this.pnlCustomize.Controls.Add(this.btnClear);
+            this.pnlCustomize.Controls.Add(this.btnRun);
+            this.pnlCustomize.Controls.Add(this.cboAlgorithms);
+            this.pnlCustomize.Controls.Add(this.lblAlgorithms);
+            this.pnlCustomize.Controls.Add(this.btnAddNote);
+            this.pnlCustomize.Controls.Add(this.numericUpDown1);
+            this.pnlCustomize.Controls.Add(this.lblItem);
+            this.pnlCustomize.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlCustomize.Location = new System.Drawing.Point(0, 0);
+            this.pnlCustomize.Name = "pnlCustomize";
+            this.pnlCustomize.Size = new System.Drawing.Size(1076, 65);
+            this.pnlCustomize.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnClear.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(849, 14);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 33);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnRun.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(756, 14);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 34);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // cboAlgorithms
+            // 
+            this.cboAlgorithms.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboAlgorithms.FormattingEnabled = true;
+            this.cboAlgorithms.Items.AddRange(new object[] {
+            "Bubble Sort",
+            "Selection Sort",
+            "Quick Sort",
+            "Insertion Sort"});
+            this.cboAlgorithms.Location = new System.Drawing.Point(575, 15);
+            this.cboAlgorithms.Name = "cboAlgorithms";
+            this.cboAlgorithms.Size = new System.Drawing.Size(159, 29);
+            this.cboAlgorithms.TabIndex = 4;
+            // 
+            // lblAlgorithms
+            // 
+            this.lblAlgorithms.AutoSize = true;
+            this.lblAlgorithms.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlgorithms.Location = new System.Drawing.Point(469, 21);
+            this.lblAlgorithms.Name = "lblAlgorithms";
+            this.lblAlgorithms.Size = new System.Drawing.Size(100, 21);
+            this.lblAlgorithms.TabIndex = 3;
+            this.lblAlgorithms.Text = "Algorithms:";
+            // 
+            // btnAddNote
+            // 
+            this.btnAddNote.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnAddNote.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNote.Location = new System.Drawing.Point(336, 14);
+            this.btnAddNote.Name = "btnAddNote";
+            this.btnAddNote.Size = new System.Drawing.Size(64, 34);
+            this.btnAddNote.TabIndex = 2;
+            this.btnAddNote.Text = "Add";
+            this.btnAddNote.UseVisualStyleBackColor = false;
+            this.btnAddNote.Click += new System.EventHandler(this.btnAddNote_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(269, 17);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(61, 28);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItem.Location = new System.Drawing.Point(205, 19);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(58, 21);
+            this.lblItem.TabIndex = 0;
+            this.lblItem.Text = "Item: ";
+            // 
+            // pnlSimulation
+            // 
+            this.pnlSimulation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlSimulation.Location = new System.Drawing.Point(0, 65);
+            this.pnlSimulation.Name = "pnlSimulation";
+            this.pnlSimulation.Size = new System.Drawing.Size(1076, 413);
+            this.pnlSimulation.TabIndex = 3;
+            // 
+            // bgwBubbleSort
+            // 
+            this.bgwBubbleSort.WorkerReportsProgress = true;
+            this.bgwBubbleSort.WorkerSupportsCancellation = true;
+            this.bgwBubbleSort.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwBubbleSort_DoWork);
+            this.bgwBubbleSort.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgwBubbleSort_ProgressChanged);
+            this.bgwBubbleSort.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwBubbleSort_RunWorkerCompleted);
+            // 
+            // bgwSelectionSort
+            // 
+            this.bgwSelectionSort.WorkerReportsProgress = true;
+            this.bgwSelectionSort.WorkerSupportsCancellation = true;
+            this.bgwSelectionSort.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwSelectionSort_DoWork);
+            this.bgwSelectionSort.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgwSelectionSort_ProgressChanged);
+            this.bgwSelectionSort.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwSelectionSort_RunWorkerCompleted);
+            // 
+            // bgwQuickSort
+            // 
+            this.bgwQuickSort.WorkerReportsProgress = true;
+            this.bgwQuickSort.WorkerSupportsCancellation = true;
+            this.bgwQuickSort.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwQuickSort_DoWork);
+            this.bgwQuickSort.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgwQuickSort_ProgressChanged);
+            this.bgwQuickSort.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwQuickSort_RunWorkerCompleted);
+            // 
+            // bgwInsertionSort
+            // 
+            this.bgwInsertionSort.WorkerReportsProgress = true;
+            this.bgwInsertionSort.WorkerSupportsCancellation = true;
+            this.bgwInsertionSort.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwInsertionSort_DoWork);
+            this.bgwInsertionSort.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgwInsertionSort_ProgressChanged);
+            this.bgwInsertionSort.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgwInsertionSort_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(233)))), ((int)(((byte)(249)))));
+            this.ClientSize = new System.Drawing.Size(1076, 478);
+            this.Controls.Add(this.pnlSimulation);
+            this.Controls.Add(this.pnlCustomize);
+            this.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Sorting";
+            this.pnlCustomize.ResumeLayout(false);
+            this.pnlCustomize.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCustomize;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.ComboBox cboAlgorithms;
+        private System.Windows.Forms.Label lblAlgorithms;
+        private System.Windows.Forms.Button btnAddNote;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.Panel pnlSimulation;
+        private System.ComponentModel.BackgroundWorker bgwBubbleSort;
+        private System.ComponentModel.BackgroundWorker bgwSelectionSort;
+        private System.ComponentModel.BackgroundWorker bgwQuickSort;
+        private System.ComponentModel.BackgroundWorker bgwInsertionSort;
+    }
+}
+
---
+++ Sorting/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting
+{
+    public partial class Form1 : Form
+    {
+        List<Button> buttons;
+        int[] arr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttons = new List<Button>();
+        }
+
+        private void btnAddNote_Click(object sender, EventArgs e)
+        {
+            int value = (int)numericUpDown1.Value;
+
+            Button btn = new Button()
+            {
+                BackColor = Content.fillColor,
+                ForeColor = Content.foreColor,
+                Font = new Font("Century Gothic", 11F),
+                Size = new Size(Content.WIDTH_SIZE, Content.HEIGHT_SIZE),
+                Text = value.ToString()
+            };
+            btn.Location = new Point(
+                Content.DISTANCE + pnlSimulation.Controls.Count * (btn.Width + Content.DISTANCE),
+                pnlSimulation.Height / 2 - btn.Height / 2);
+            pnlSimulation.Controls.Add(btn);
+            buttons.Add(btn);
+
+            btnRun.Enabled = pnlSimulation.Controls.Count >= 2;
+
+            btnAddNote.Enabled = pnlSimulation.Controls.Count < 10;
+            this.AcceptButton = pnlSimulation.Controls.Count < 10 ? btnAddNote : null;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            arr = new int[buttons.Count];
+            for (int i = 0; i < buttons.Count; i++)
+                arr[i] = Convert.ToInt32(buttons[i].Text);
+
+            if (cboAlgorithms.SelectedIndex == 0)
+                bgwBubbleSort.RunWorkerAsync();
+            else if (cboAlgorithms.SelectedIndex == 1)
+                bgwSelectionSort.RunWorkerAsync();
+            else if (cboAlgorithms.SelectedIndex == 2)
+                bgwQuickSort.RunWorkerAsync();
+            else if (cboAlgorithms.SelectedIndex == 3)
+                bgwInsertionSort.RunWorkerAsync();
+            btnAddNote.Enabled = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            buttons = new List<Button>();
+            pnlSimulation.Controls.Clear();
+        }
+        private void Swap(ref int num1, ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        private void MoveButtons(BackgroundWorker bgw, int local1, int local2)
+        {
+            Status st = new Status();
+            st.Local1 = local1;
+            st.Local2 = local2;
+
+            st.Type = MoveType.UP_DOWN;
+            for (int i = 0; i < Content.HEIGHT; i++)
+            {
+                bgw.ReportProgress(0, st);
+                Thread.Sleep(Content.threadTime);
+            }
+
+            st.Type = MoveType.RIGHT_LEFT;
+            int WIDTH = Math.Abs(local1 - local2) * (Content.WIDTH_SIZE + Content.DISTANCE);
+            for (int i = 0; i < WIDTH; i++)
+            {
+                bgw.ReportProgress(0, st);
+                Thread.Sleep(Content.threadTime);
+            }
+
+            st.Type = MoveType.DOWN_UP;
+            for (int i = 0; i < Content.HEIGHT; i++)
+            {
+                bgw.ReportProgress(0, st);
+                Thread.Sleep(Content.threadTime);
+            }
+
+            st.Type = MoveType.STOP;
+            bgw.ReportProgress(0, st);
+        }
+
+        private void ProgressButtons(Status st, string moveType)
+        {
+            if (st == null) return;
+            if (st.Type == MoveType.STOP)
+            {
+                Button btn = buttons[st.Local2];
+                buttons[st.Local2] = buttons[st.Local1];
+                buttons[st.Local1] = btn;
+                return;
+            }
+
+            Button btn1 = buttons[st.Local1];
+            Button btn2 = buttons[st.Local2];
+            if (st.Type == MoveType.UP_DOWN)
+            {
+                btn1.Top += 1;
+                btn2.Top -= 1;
+            }
+            else if (st.Type == MoveType.RIGHT_LEFT)
+            {
+                if (moveType == "RL")
+                {
+                    btn1.Left -= 1;
+                    btn2.Left += 1;
+                }
+                else
+                {
+                    btn1.Left += 1;
+                    btn2.Left -= 1;
+                }
+            }
+            else if (st.Type == MoveType.DOWN_UP)
+            {
+                btn1.Top -= 1;
+                btn2.Top += 1;
+            }
+        }
+
+        private void RunWorkerCompletedButtons()
+        {
+            foreach (Button btn in pnlSimulation.Controls)
+            {
+                btn.BackColor = Content.fillColorFinish;
+                btn.ForeColor = Content.foreColorFinish;
+            }
+            btnAddNote.Enabled = true;
+            btnRun.Enabled = false;
+            btnClear.Enabled = true;
+        }
+        private void BubbleSort(int[] arr)
+        {
+            Status st = new Status();
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = arr.Length - 1; j > i; j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        Swap(ref arr[j], ref arr[j - 1]);
+                        Thread.Sleep(Content.threadTime);
+                        MoveButtons(bgwBubbleSort, j, j - 1);
+                    }
+                }
+        }
+
+        private void bgwBubbleSort_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BubbleSort(arr);
+        }
+
+        private void bgwBubbleSort_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressButtons(e.UserState as Status, "RL");
+        }
+
+        private void bgwBubbleSort_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            RunWorkerCompletedButtons();
+        }
+        private void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                        min = j;
+                }
+                Swap(ref arr[min], ref arr[i]);
+                Thread.Sleep(Content.threadTime);
+                MoveButtons(bgwSelectionSort, min, i);
+            }
+        }
+
+        private void bgwSelectionSort_DoWork(object sender, DoWorkEventArgs e)
+        {
+            SelectionSort(arr);
+        }
+
+        private void bgwSelectionSort_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressButtons(e.UserState as Status, "RL");
+        }
+
+        private void bgwSelectionSort_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            RunWorkerCompletedButtons();
+        }
+        private void QuickSort(int[] arr, int left, int right)
+        {
+            int i = left, j = right;
+            int mid = arr[(i + j) / 2];
+            do
+            {
+                while (arr[i] < mid) i++;
+                while (arr[j] > mid) j--;
+
+                if (i <= j)
+                {
+                    Swap(ref arr[i], ref arr[j]);
+                    Thread.Sleep(Content.threadTime);
+                    MoveButtons(bgwQuickSort, i, j);
+                    i++; j--;
+                }
+            }
+            while (i < j);
+            if (left < j) QuickSort(arr, left, j);
+            if (i < right) QuickSort(arr, i, right);
+        }
+
+        private void bgwQuickSort_DoWork(object sender, DoWorkEventArgs e)
+        {
+            QuickSort(arr, 0, arr.Length - 1);
+        }
+
+        private void bgwQuickSort_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressButtons(e.UserState as Status, "LR");
+        }
+
+        private void bgwQuickSort_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            RunWorkerCompletedButtons();
+        }
+        private void InsertionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        Swap(ref arr[j - 1], ref arr[j]);
+                        Thread.Sleep(Content.threadTime);
+                        MoveButtons(bgwInsertionSort, j - 1, j);
+                    }
+                }
+        }
+
+        private void bgwInsertionSort_DoWork(object sender, DoWorkEventArgs e)
+        {
+            InsertionSort(arr);
+        }
+
+        private void bgwInsertionSort_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressButtons(e.UserState as Status, "LR");
+        }
+
+        private void bgwInsertionSort_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            RunWorkerCompletedButtons();
+        }
+    }
+    public class Content
+    {
+        public static int HEIGHT = 100;
+        public static int WIDTH_SIZE = 60;
+        public static int HEIGHT_SIZE = 50;
+        public static int DISTANCE = 20;
+
+
+        public static int RANDOM_START = 0;
+        public static int RANDOM_END = 100;
+
+
+        public static Color fillColor = Color.FromArgb(255, 120, 80);
+        public static Color foreColor = Color.White;
+        public static Color fillColorFinish = Color.FromArgb(255, 239, 100);
+        public static Color foreColorFinish = Color.Black;
+
+
+        public static int threadTime = 1;
+    }
+    public enum MoveType { UP_DOWN, RIGHT_LEFT, DOWN_UP, STOP }
+
+    public class Status
+    {
+        public MoveType Type { get; set; }
+        public int Local1 { get; set; }
+        public int Local2 { get; set; }
+    }
+}
